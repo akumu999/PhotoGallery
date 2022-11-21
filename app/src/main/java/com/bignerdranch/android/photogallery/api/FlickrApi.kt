3f15178508a3f4d428fbe36ecf59e8c0a0f97dc1
akumu999 <snow.lite@mail.ru>
2022-11-22 00:54:58 +0500

    первая фиксация

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.photogallery.api
+
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface FlickrApi {
+    @GET("services/rest/?method=flickr.interestingness.getList" + "&api_key=56bbc5d158e5c92dc817ab2c313e1dd4\n")
+    fun fetchPhotos(): Call<FlickrResponse>
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
+}
+
